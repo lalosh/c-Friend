aa62ad1274e930dae39b606663ed1723a0b99d4e
محمد لؤي العش <louayalosh17@gmail.com>
2017-09-04 21:33:21 +0300

    تحميل زائد

---
+++ class3.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+class engineer{
+public:
+    int mark;
+    
+public:
+    engineer(){
+        mark = 0;
+    }
+    ~engineer(){
+
+    }
+    engineer(float array[],int size){
+        mark = 0;
+        for(int i = 0; i < size; i++){
+            if(array[i] >= 60)
+                mark++;
+        }
+    }
+    int getMark(){
+        return mark;
+    }
+    engineer operator-(engineer outside){
+        engineer t;
+        t.mark = mark - outside.getMark();
+        return t;
+    }
+    engineer operator+(engineer outside){
+        engineer t;
+        t.mark = mark + outside.getMark();
+        return t;
+    }
+
+};
+
+int counter(engineer e){
+    int t = e.getMark();
+    return t;
+}
+
+int main(){
+
+    float array1[] = {1,255,3,24,5,655};
+    float array2[] = {14,200,37,4,51,65,100};
+    
+    engineer e(array1, 6);
+    engineer m(array2, 7);
+
+    engineer t;
+    
+    t = e + m;
+
+    cout << e.getMark() << endl;
+    cout << m.getMark() << endl;   
+    cout << t.getMark() << endl;
+    
+}
